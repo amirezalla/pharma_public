7468036e4f9cc6ca9d6afe1df68c5c7a5cbcdad3
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-03-06 10:52:03 +0200

    Update product.blade.php

--- platform/themes/wowy/views/ecommerce/product.blade.php
+++ platform/themes/wowy/views/ecommerce/product.blade.php
@@ -53,8 +53,6 @@
                         $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
                         curl_close($ch);
 
-                        dd($defaultImgUrl);
-
                         if ($responseCode == 200) {
                             $Image = $productImgUrl;
                         } else {
