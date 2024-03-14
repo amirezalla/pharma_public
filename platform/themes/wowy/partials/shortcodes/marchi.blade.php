786a79cb9731f201923c9e091f7ef85761215a76
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-03-14 12:23:43 +0200

    Update marchi.blade.php

--- platform/themes/wowy/partials/shortcodes/marchi.blade.php
+++ platform/themes/wowy/partials/shortcodes/marchi.blade.php
@@ -2,21 +2,24 @@
     <div class="row">
         @dd($shortcode)
         @for ($i = 1; $i <= 20; $i++)
-            <div class="col-md-3 mb-4">
-                <div class="card">
-                    <img src="{{ $shortcode->{'foto' . $i} }}" class="card-img-top" alt="{{ $shortcode->{'title' . $i} }}">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $shortcode->{'title' . $i} }}</h5>
-                        <hr>
+            @if (isset($shortcode->attributes['foto' . $i]))
+                <div class="col-md-3 mb-4">
+                    <div class="card">
+                        <img src="{{ $shortcode->attributes['foto' . $i] }}" class="card-img-top"
+                            alt="{{ $shortcode->attributes['title' . $i] }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $shortcode->attributes['title' . $i] }}</h5>
+                            <hr>
 
-                        <a href="{{ $shortcode->{'catalog' . $i} }}" target="_blank"
-                            class="btn btn-md btn-outline mt-1">SCARICA IL CATALOGO <i
-                                class="fa fa-download m-1"></i></a>
-                        <a href="https://marigopharma.it/contact" class="btn btn-md btn-outline mt-1">RICHIEDI
-                            INFORMAZIONI <i class="fa fa-envelope m-1"></i></a>
+                            <a href="{{ $shortcode->attributes['catalog' . $i] }}" target="_blank"
+                                class="btn btn-md btn-outline mt-1">SCARICA IL CATALOGO <i
+                                    class="fa fa-download m-1"></i></a>
+                            <a href="https://marigopharma.it/contact" class="btn btn-md btn-outline mt-1">RICHIEDI
+                                INFORMAZIONI <i class="fa fa-envelope m-1"></i></a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endif
         @endfor
     </div>
 </div>
