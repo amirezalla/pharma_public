07048e8c5f0fed6558332a4504099a4d350e38b7
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-03-14 12:16:24 +0200

    Update marchi-admin-config.blade.php

--- platform/themes/wowy/partials/shortcodes/marchi-admin-config.blade.php
+++ platform/themes/wowy/partials/shortcodes/marchi-admin-config.blade.php
@@ -1,15 +1,18 @@
 @for ($i = 1; $i < 20; $i++)
-    <div class="form-group">
-        <label class="control-label">{{ __('Title') }}</label>
-        <input type="text" name="title" value="{{ Arr::get($attributes, 'title') }}" class="form-control"
-            placeholder="Title">
+    <div class="mt-5 mb-5" style="padding: 10px;background-color:rgba(201, 201, 201, 0.629);">
+        <div class="form-group">
+            <label class="control-label">{{ __('Titolo :number', ['number' => $i]) }}</label>
+            <input type="text" name="title" value="{{ Arr::get($attributes, 'title' . $i)) }}" class="form-control"
+                placeholder="Title">
+        </div>
+        <div class="form-group">
+            <label class="control-label">{{ __('Foto :number', ['number' => $i]) }}</label>
+            {!! Form::mediaImage('foto' . $i, Arr::get($attributes, 'foto' . $i)) !!}
+        </div>
+        <div class="form-group">
+            <label class="control-label">{{ __('Catalog :number', ['number' => $i]) }}</label>
+            {!! Form::mediaFile('catalog' . $i, Arr::get($attributes, 'catalog' . $i)) !!}
+        </div>
     </div>
-    <div class="form-group">
-        <label class="control-label">{{ __('Foto :number', ['number' => $i]) }}</label>
-        {!! Form::mediaImage('foto' . $i, Arr::get($attributes, 'foto' . $i)) !!}
-    </div>
-    <div class="form-group">
-        <label class="control-label">{{ __('Catalog :number', ['number' => $i]) }}</label>
-        {!! Form::mediaFile('catalog' . $i, Arr::get($attributes, 'catalog' . $i)) !!}
-    </div>
+    
 @endfor
