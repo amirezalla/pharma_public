3fb22ffeb7682d003a85f8e0b4a084a7da48bd82
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-03-14 12:21:01 +0200

    Update marchi-admin-config.blade.php

--- platform/themes/wowy/partials/shortcodes/marchi-admin-config.blade.php
+++ platform/themes/wowy/partials/shortcodes/marchi-admin-config.blade.php
@@ -2,8 +2,8 @@
     <div class="mt-5 mb-5" style="padding: 10px;background-color:rgba(201, 201, 201, 0.629);">
         <div class="form-group">
             <label class="control-label">{{ __('Titolo :number', ['number' => $i]) }}</label>
-            <input type="text" name="title" value="{{ Arr::get($attributes, 'title' . $i) }}" class="form-control"
-                placeholder="Title">
+            <input type="text" name="title{{ $i }}" value="{{ Arr::get($attributes, 'title' . $i) }}"
+                class="form-control" placeholder="Title">
         </div>
         <div class="form-group">
             <label class="control-label">{{ __('Foto :number', ['number' => $i]) }}</label>
