8272ca75eadfbe927e3806b8abe39ceb0764a20b
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-03-14 12:25:44 +0200

    Update marchi.blade.php

--- platform/themes/wowy/partials/shortcodes/marchi.blade.php
+++ platform/themes/wowy/partials/shortcodes/marchi.blade.php
@@ -1,17 +1,16 @@
 <div class="container mt-5">
     <div class="row">
-        @dd($shortcode->attributes)
+
         @for ($i = 1; $i <= 20; $i++)
-            @if (isset($shortcode->attributes['title' . $i]))
+            @if (isset($shortcode['title' . $i]))
                 <div class="col-md-3 mb-4">
                     <div class="card">
-                        <img src="{{ $shortcode->attributes['foto' . $i] }}" class="card-img-top"
-                            alt="{{ $shortcode->attributes['title' . $i] }}">
+                        <img src="{{ $shortcode['foto' . $i] }}" class="card-img-top" alt="{{ $shortcode['title' . $i] }}">
                         <div class="card-body">
-                            <h5 class="card-title">{{ $shortcode->attributes['title' . $i] }}</h5>
+                            <h5 class="card-title">{{ $shortcode['title' . $i] }}</h5>
                             <hr>
 
-                            <a href="{{ $shortcode->attributes['catalog' . $i] }}" target="_blank"
+                            <a href="{{ $shortcode['catalog' . $i] }}" target="_blank"
                                 class="btn btn-md btn-outline mt-1">SCARICA IL CATALOGO <i
                                     class="fa fa-download m-1"></i></a>
                             <a href="https://marigopharma.it/contact" class="btn btn-md btn-outline mt-1">RICHIEDI
