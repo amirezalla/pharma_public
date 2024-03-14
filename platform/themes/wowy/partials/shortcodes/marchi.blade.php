8f484180f984b05c6878c279011766e7e0a8bda6
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-03-14 12:19:14 +0200

    Update marchi.blade.php

--- platform/themes/wowy/partials/shortcodes/marchi.blade.php
+++ platform/themes/wowy/partials/shortcodes/marchi.blade.php
@@ -2,24 +2,21 @@
     <div class="row">
 
         @for ($i = 1; $i <= 20; $i++)
-            @if ($shortcode->{'title' . $i})
-                <div class="col-md-3 mb-4">
-                    <div class="card">
-                        <img src="{{ $shortcode->{'foto' . $i} }}" class="card-img-top"
-                            alt="{{ $shortcode->{'title' . $i} }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $shortcode->{'title' . $i} }}</h5>
-                            <hr>
+            <div class="col-md-3 mb-4">
+                <div class="card">
+                    <img src="{{ $shortcode->{'foto' . $i} }}" class="card-img-top" alt="{{ $shortcode->{'title' . $i} }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $shortcode->{'title' . $i} }}</h5>
+                        <hr>
 
-                            <a href="{{ $shortcode->{'catalog' . $i} }}" target="_blank"
-                                class="btn btn-md btn-outline mt-1">SCARICA IL CATALOGO <i
-                                    class="fa fa-download m-1"></i></a>
-                            <a href="https://marigopharma.it/contact" class="btn btn-md btn-outline mt-1">RICHIEDI
-                                INFORMAZIONI <i class="fa fa-envelope m-1"></i></a>
-                        </div>
+                        <a href="{{ $shortcode->{'catalog' . $i} }}" target="_blank"
+                            class="btn btn-md btn-outline mt-1">SCARICA IL CATALOGO <i
+                                class="fa fa-download m-1"></i></a>
+                        <a href="https://marigopharma.it/contact" class="btn btn-md btn-outline mt-1">RICHIEDI
+                            INFORMAZIONI <i class="fa fa-envelope m-1"></i></a>
                     </div>
                 </div>
-            @endif
+            </div>
         @endfor
     </div>
 </div>
