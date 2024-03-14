bf48305bde83977643217819def5a69b75cee01e
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-03-14 12:09:52 +0200

    marchi shotcode

--- platform/themes/wowy/partials/shortcodes/marchi-admin-config.blade.php
+++ platform/themes/wowy/partials/shortcodes/marchi-admin-config.blade.php
@@ -1,5 +1,15 @@
-<div class="form-group">
-    <label class="control-label">{{ __('Title') }}</label>
-    <input type="text" name="title" value="{{ Arr::get($attributes, 'title') }}" class="form-control" placeholder="Title">
-</div>
-
+@for ($i = 1; $i < 20; $i++)
+    <div class="form-group">
+        <label class="control-label">{{ __('Title') }}</label>
+        <input type="text" name="title" value="{{ Arr::get($attributes, 'title') }}" class="form-control"
+            placeholder="Title">
+    </div>
+    <div class="form-group">
+        <label class="control-label">{{ __('Foto :number', ['number' => $i]) }}</label>
+        {!! Form::mediaImage('foto' . $i, Arr::get($attributes, 'foto' . $i)) !!}
+    </div>
+    <div class="form-group">
+        <label class="control-label">{{ __('Catalog :number', ['number' => $i]) }}</label>
+        {!! Form::mediaImage('catalog' . $i, Arr::get($attributes, 'catalog' . $i)) !!}
+    </div>
+@endfor
