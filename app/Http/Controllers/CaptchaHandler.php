fa080929f21c856b37b78d2ae02bcf245e1ae5f6
Amirezalla <allahverdiamirreza@gmail.com>
2025-01-13 21:03:15 +0100

    Update CaptchaHandler.php

--- app/Http/Controllers/CaptchaHandler.php
+++ app/Http/Controllers/CaptchaHandler.php
@@ -17,8 +17,7 @@ class CaptchaHandler extends BaseController
     public static function validateLoginForm1($captcha){
         $userCaptchaResponse = intval($captcha);
         $decryptedAnswer = intval(Crypt::decryptString(session('login_form_captcha_answer')));
-        dd($userCaptchaResponse,$decryptedAnswer);
-        if ($userCaptchaResponse == $decryptedAnswer) {
+        if ($userCaptchaResponse === $decryptedAnswer) {
             return true;
         } else {
             // Optionally, you could add more information here to help debug the issue
